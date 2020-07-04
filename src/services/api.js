d83b538597c4d186d1bfa520809daf1b1a479e20
thiagoTL <thiago.lourenco@deway.com.br>
2020-07-04 00:10:37 -0300

    Primeiro commit

---
+++ src/services/api.js
@@ -0,0 +1,105 @@
+export function loadLists() {
+  return [
+    {
+      title: "Tarefas",
+      creatable: true,
+      cards: [
+        {
+          id: 1,
+          content: "Estudar GraphQL, para melhorar performace APP",
+          labels: ["#222222"],
+          user:
+            "https://api.adorable.io/avatars/285/abott@adorable.pngCopy to Clipboard",
+        },
+        {
+          id: 2,
+          content: "Tirar Foto",
+          labels: ["#222222"],
+          user:
+            "https://api.adorable.io/avatars/285/abott@adorable.pngCopy to Clipboard",
+        },
+        {
+          id: 3,
+          content: "Estudar React Native",
+          labels: ["#222222"],
+          user:
+            "https://api.adorable.io/avatars/285/abott@adorable.pngCopy to Clipboard",
+        },
+        {
+          id: 4,
+          content: "Densevolver Edição de senha, na tela de profile",
+          labels: ["#54e1f7"],
+          user:
+            "https://api.adorable.io/avatars/285/abott@adorable.pngCopy to Clipboard",
+        },
+        {
+          id: 5,
+          content: "Densevolver Edição de senha, na tela de profile",
+          labels: ["#54e1f7"],
+          user:
+            "https://api.adorable.io/avatars/285/abott@adorable.pngCopy to Clipboard",
+        },
+      ],
+    },
+    {
+      title: "Fazendo",
+      creatable: false,
+      cards: [
+        {
+          id: 6,
+          content: "Densevolver Edição de senha, na tela de profile",
+          labels: [],
+          user:
+            "https://api.adorable.io/avatars/285/abott@adorable.pngCopy to Clipboard",
+        },
+      ],
+    },
+    {
+      title: "Pausado",
+      creatable: false,
+      cards: [
+        {
+          id: 7,
+          content: "Densevolver Edição de senha, na tela de profile",
+          labels: ["#222222"],
+          user:
+            "https://api.adorable.io/avatars/285/abott@adorable.pngCopy to Clipboard",
+        },
+        {
+          id: 8,
+          content: "Densevolver Edição de senha, na tela de profile",
+          labels: ["#54e1f7"],
+          user:
+            "https://api.adorable.io/avatars/285/abott@adorable.pngCopy to Clipboard",
+        },
+        {
+          id: 9,
+          content: "Densevolver Edição de senha, na tela de profile",
+          labels: [],
+        },
+      ],
+    },
+    {
+      title: "Concluído",
+      creatable: false,
+      done: true,
+      cards: [
+        {
+          id: 10,
+          content: "Densevolver Edição de senha, na tela de profile",
+          labels: [],
+        },
+        {
+          id: 12,
+          content: "Densevolver Edição de senha, na tela de profile",
+          labels: ["#54e1f7"],
+        },
+        {
+          id: 13,
+          content: "Densevolver Edição de senha, na tela de profile",
+          labels: ["#222222"],
+        },
+      ],
+    },
+  ];
+}
